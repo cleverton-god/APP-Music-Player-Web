5c20add0f4265f58c736c0c7f21eae6a95a35072
cleverton <cleverton_rosa150@estudante.sesisenai.org.br>
2025-11-16 18:19:56 -0300

    # versão teste

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'playlist-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  // coloque aqui suas músicas, CSS e JS
+  '/style.css',
+  '/script.js'
+];
+
+// Instala o service worker e faz cache dos arquivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Ativa o service worker e remove caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Intercepta requisições e serve do cache quando possível
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(resp => resp || fetch(event.request))
+  );
+});
